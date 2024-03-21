81651af754a7d8ffbc35c1f2bd7876a2b3121df0
ffst4irs@gmail.com <ffst4irs@gmail.com>
2024-03-22 05:44:02 +0700

    Continue building the home page.

--- src/scripts/global.js
+++ src/scripts/global.js
@@ -23,3 +23,33 @@ window.addEventListener("scroll", () => {
 ScrollToTopButton.addEventListener("click", () => {
   window.scrollTo({ top: 0, behavior: "smooth" });
 });
+
+// Dropdown
+const DropdownButton = document.getElementById("dropdown-button");
+const DropdownMenu = document.getElementById("dropdown-menu");
+let open = false;
+
+document.body.addEventListener("click", (e) => {
+  if (!DropdownButton.contains(e.target)) {
+    DropdownMenu.classList.add("opacity-0");
+    open = false;
+  }
+});
+
+DropdownButton.addEventListener("click", () => {
+  open = !open;
+  if (open) {
+    DropdownMenu.classList.remove("opacity-0");
+  } else {
+    DropdownMenu.classList.add("opacity-0");
+  }
+});
+
+const DropdownMediaQuery = () => {
+  if (window.matchMedia("(min-width: 863px)").matches) {
+    DropdownMenu.classList.add("opacity-0");
+    open = false;
+  }
+};
+
+window.addEventListener("resize", DropdownMediaQuery);
