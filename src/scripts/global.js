486baf3652f275d0c6a716a2b321f1b5e13acbfd
ffst4irs@gmail.com <ffst4irs@gmail.com>
2024-04-05 20:37:48 +0700

    Major fix improvements.

--- src/scripts/global.js
+++ src/scripts/global.js
@@ -25,28 +25,51 @@ ScrollToTopButton.addEventListener("click", () => {
 });
 
 // Dropdown
-// const DropdownButton = document.getElementById("dropdown-button");
-// const DropdownMenu = document.getElementById("dropdown-menu");
-// let open = false;
+const DropdownButton = document.getElementById("dropdown-button");
+const DropdownMenu = document.getElementById("dropdown-menu");
+let open = false;
 
-// document.body.addEventListener("click", (e) => {
-//   if (!DropdownButton.contains(e.target)) {
-//     DropdownMenu.classList.add("d-none");
-//     open = false;
-//   } else if (!open) {
-//     open = true;
-//     DropdownMenu.classList.remove("d-none");
-//   }
-// });
+DropdownMenu.addEventListener("click", (e) => {
+  if (e.target.id === "beranda-button") {
+    BerandaSection.scrollIntoView({
+      behavior: "smooth",
+    });
+  }
+  if (e.target.id === "perjuangan-button") {
+    PerjuanganSection.scrollIntoView({
+      behavior: "smooth",
+    });
+  }
+  if (e.target.id === "pahlawan-button") {
+    PahlawanSection.scrollIntoView({
+      behavior: "smooth",
+    });
+  }
+  if (e.target.id === "saat-ini-button") {
+    SaatIniSection.scrollIntoView({
+      behavior: "smooth",
+    });
+  }
+});
 
-// DropdownButton.addEventListener("click", () => {
-//   open = !open;
-//   if (open) {
-//     DropdownMenu.classList.remove("d-none");
-//   } else {
-//     DropdownMenu.classList.add("d-none");
-//   }
-// });
+document.body.addEventListener("click", (e) => {
+  if (!DropdownButton.contains(e.target)) {
+    DropdownMenu.classList.add("d-none");
+    open = false;
+  } else if (!open) {
+    open = true;
+    DropdownMenu.classList.remove("d-none");
+  }
+});
+
+DropdownButton.addEventListener("click", () => {
+  open = !open;
+  if (open) {
+    DropdownMenu.classList.remove("d-none");
+  } else {
+    DropdownMenu.classList.add("d-none");
+  }
+});
 
 const DropdownMediaQuery = () => {
   if (window.matchMedia("(min-width: 863px)").matches) {
@@ -76,68 +99,32 @@ let clickToView = (section) => {
 }
 
 const BerandaButton = document.getElementById("beranda-button");
-const BerandaBtnMobile = document.getElementById("beranda-button-mobile");
 const BerandaSection = document.getElementById("beranda-section");
-
 
 BerandaButton.addEventListener("click", () => {
   clickToView(BerandaSection);
-});
-BerandaBtnMobile.addEventListener("click", () => {
-  clickToView(BerandaSection);
-});
+})
 
 const PerjuanganButton = document.getElementById("perjuangan-button");
-const PerjuanganBtnMobile = document.getElementById("perjuangan-button-mobile");
 const PerjuanganSection = document.getElementById("perjuangan-section");
-
 
 PerjuanganButton.addEventListener("click", () => {
   clickToView(PerjuanganSection);
-});
-PerjuanganBtnMobile.addEventListener("click", () => {
-  clickToView(PerjuanganSection);
-});
+})
 
 const PahlawanButton = document.getElementById("pahlawan-button");
-const PahlawanBtnMobile = document.getElementById("pahlawan-button-mobile");
 const PahlawanSection = document.getElementById("pahlawan-section");
 
 PahlawanButton.addEventListener("click", () => {
   clickToView(PahlawanSection);
-});
-PahlawanBtnMobile.addEventListener("click", () => {
-  clickToView(PahlawanSection);
-});
-
-const saatIniButton = document.getElementById("saat-ini-button");
-const saatIniBtnMobile = document.getElementById("saat-ini-button-mobile");
-const saatIniSection = document.getElementById("saat-ini-section");
-
-saatIniButton.addEventListener("click", () => {
-  clickToView(saatIniSection);
-})
-saatIniBtnMobile.addEventListener("click", () => {
-  clickToView(saatIniSection);
 })
 
-DropdownMenu.addEventListener("click", (e) => {
-  if (e.target.id === "beranda-button") {
-    BerandaSection.scrollIntoView({
-      behavior: "smooth",
-    });
-  }
-  if (e.target.id === "perjuangan-button") {
-    PerjuanganSection.scrollIntoView({
-      behavior: "smooth",
-    });
-  }
-  if (e.target.id === "pahlawan-button") {
-    PahlawanSection.scrollIntoView({
-      behavior: "smooth",
-    });
-  }
-});
+const SaatIniButton = document.getElementById("saat-ini-button");
+const SaatIniSection = document.getElementById("saat-ini-section");
+
+SaatIniButton.addEventListener("click", () => {
+  clickToView(SaatIniSection);
+})
 
 // Karosel 1
 document.addEventListener("DOMContentLoaded", () => {
